d9b9a6ddd741f591eaa1fed3dc0e7b0127626087
liveon032 <126222078+liveon032@users.noreply.github.com>
2023-02-24 20:56:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,32 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#new
+#smartdns
+svn co https://github.com/pymumu/openwrt-smartdns package/smartdns
+svn co https://github.com/pymumu/luci-app-smartdns/branches/lede package/luci-app-smartdns
+
+Openclash
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash package/luci-app-openclash
+
+#adguardhome
+svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+
+
+# openwrt_helloworld
+svn co  https://github.com/sbwml/openwrt_helloworld package/helloworld
+
+
+# default on dhcp.lan.force
+sed -i '/exit 0/d' package/lean/default-settings/files/zzz-default-settings
+echo 'uci set dhcp.lan.force=1' >>package/lean/default-settings/files/zzz-default-settings
+echo 'uci commit dhcp' >>package/lean/default-settings/files/zzz-default-settings
+echo 'exit 0' >>package/lean/default-settings/files/zzz-default-settings
+
